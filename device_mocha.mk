b23e2a2efa46d0e05b92007d89c992acddb0245f
harrynowl@ubuntu16.04 <harrynowl@xda>
2016-12-09 15:24:56 +0000

    Initial device configuration

--- device_mocha.mk
+++ device_mocha.mk
@@ -22,3 +22,24 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_mocha
 PRODUCT_DEVICE := mocha
+
+# Screen density
+PRODUCT_AAPT_CONFIG += xlarge large
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1536
+
+# Platform
+PRODUCT_CHARACTERISTICS := tablet
+TARGET_TEGRA_VERSION := t124
+TARGET_TEGRA_MODEM := icera
+TARGET_TEGRA_TOUCH := raydium
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
+# Proprietary vendor files
+$(call inherit-product-if-exists, vendor/xiaomi/mocha/mocha-vendor.mk)
