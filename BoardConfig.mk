8718f068ffe0b1dbc394a2b662e0ee4516dfa137
harrynowl@ubuntu16.04 <harrynowl@xda>
2016-12-12 21:30:29 +0000

    mocha: Basic init
    Change-Id: Id16f779b0d51ea7d5f0c514ac87a40489b44d433

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := mocha
 
-BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
+BOARD_KERNEL_CMDLINE := "androidboot.selinux=disabled"
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --dt device/xiaomi/mocha/dt.img --tags_offset 0x00000100
